8b2fec86de9f9b7c69bda2ebaaaec605e6a596df
Nursultan <nursultan_imanov@mail.ru>
2019-07-26 17:49:45 +0200

    R2D11

---
+++ leetcode/626.exchange-seats.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select (case when id%2<>0 and counts!=id then id+1 when id%2<>0 and counts=id then id else id-1 end) as id,student from seat, (select count(*) as counts from seat) as seat_counts ORDER BY id ASC;
